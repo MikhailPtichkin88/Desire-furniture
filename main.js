234b92c3825a621d173dfec968a172889f8631fa
MikhailPtichkin88 <95014280+MikhailPtichkin88@users.noreply.github.com>
2022-03-02 00:04:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+$(function () {
+
+$('.header__btn').on('click', function() {
+    $('.rightside-menu').removeClass('rightside-menu--close');
+});
+$('.rightside-menu__close').on('click', function() {
+    $('.rightside-menu').addClass('rightside-menu--close');
+});
+
+$('.header__btn-menu').on('click', function() {
+    $('.menu').toggleClass('menu--open');
+});
+
+
+if($(window).width() < 651){
+    $('.works-path__item--measuring').appendTo($('.works-path__item-box'));
+}
+
+$('.top__slider').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: false,
+
+});
+
+
+$(".contact-slider").slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    dots: true,
+    arrows: false,
+    responsive: [
+        {
+          breakpoint: 1700,
+          settings: {
+            slidesToShow: 8,
+            slidesToScroll: 8,
+          }
+        },
+        {
+          breakpoint: 1511,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 6,
+          }
+        },
+        {
+            breakpoint: 1201,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+            }
+          },
+          {
+            breakpoint: 841,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 551,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },
+          {
+            breakpoint: 376,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          }
+      ]
+});
+
+
+$(".article-slider-box").slick({
+   
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="images/blog/slider-left.svg" alt="arrow"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="images/blog/slider-right.svg" alt="arrow"></button>'
+      
+});
+
+var mixer = mixitup('.gallery__inner', {
+    load: {
+        filter: '.bedroom'
+    }
+});
+
+});
